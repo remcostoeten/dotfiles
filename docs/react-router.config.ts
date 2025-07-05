82555f2d70add109633d873eca2d478d729dbdec
Remco Stoeten <remcostoeten@hotmail.com>
2025-07-05 21:53:31 +0200

    Remove docs

--- docs/react-router.config.ts
+++
@@ -1,9 +0,0 @@
-import type { Config } from '@react-router/dev/config';
-import { source } from './app/source';
-
-export default {
-  ssr: true,
-  async prerender({ getStaticPaths }) {
-    return [...getStaticPaths(), ...source.getPages().map((page) => page.url)];
-  },
-} satisfies Config;
