e77f41148ae59ffd3beba3b099cd9d469fea7f3e
Remco Stoeten <remcostoeten@hotmail.com>
2025-10-28 18:47:33 +0100

    refactor: update drizzle aliases for clarity and simplicity
    - Replace 'dkp' alias with 'drizzle' for better readability
    - Remove unnecessary comments to streamline the alias file

--- configs/fish/aliases/drizzle.fish
+++ configs/fish/aliases/drizzle.fish
@@ -1,12 +1,8 @@
 #!/usr/bin/env fish
 
-# Drizzle ORM aliases
-
 # DOCSTRING: drizzle kit  
 alias dk='drizzle-kit'
-
-# DOCSTRING: push drizzle
-alias dkp='drizzle-kit push'
+alias drizzle='drizzle-kit'
 
 # DOCSTRING: generate schema
 alias gen='drizzle-kit generate'
