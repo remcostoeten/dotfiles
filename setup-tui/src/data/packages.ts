e3978f639d7fa13f0238258d4fbe823a1bcd3922
Remco Stoeten <remcostoeten@hotmail.com>
2025-11-02 20:28:12 +0100

    x

--- setup-tui/src/data/packages.ts
+++
@@ -1,6 +0,0 @@
-import { importFromBash } from "./packageImporter";
-import type { Package } from "./packageImporter";
-
-export type { Package };
-
-export const packages: Package[] = importFromBash();
