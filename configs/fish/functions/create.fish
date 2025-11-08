982ce47cf032f730314d3b4df040b36d1e365da6
Remco Stoeten <remcostoeten@hotmail.com>
2025-11-08 14:07:57 +0100

    Use DOTFILES_DIR global variable instead of hardcoded path
    - Script now uses DOTFILES_DIR environment variable with fallback
    - Fish wrapper also uses DOTFILES_DIR
    - History directory now uses $DOTFILES_DIR/create instead of hardcoded path
    - More flexible and consistent with rest of dotfiles infrastructure

--- configs/fish/functions/create.fish
+++ configs/fish/functions/create.fish
@@ -1,7 +1,9 @@
 function create
     # Wrapper for the create script to work in fish
     # The actual implementation is in scripts/create
-    set -l script_path ~/.config/dotfiles/scripts/create
+    # Use DOTFILES_DIR if set, otherwise default to ~/.config/dotfiles
+    set -l dotfiles_dir (test -n "$DOTFILES_DIR" && echo "$DOTFILES_DIR" || echo ~/.config/dotfiles)
+    set -l script_path "$dotfiles_dir/scripts/create"
     
     if test -f "$script_path"
         # Execute the script with all arguments
@@ -11,4 +13,3 @@ function create
         return 1
     end
 end
-
